f2ca678e6b302d7d9c0ce5aaba1206d6947a6ddc
Disa A. Syuhada <disaada@gmail.com>
2020-09-23 11:52:27 +0700

    beautify notify UI

--- client/src/pages/RegistrationNotice.jsx
+++ client/src/pages/RegistrationNotice.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import '../styles/notice.css'
 
 function RegistrationNotice () {
 	return (
 		<div>
-			RegistrationNotice
+			<main role="main" class="inner cover">
+			    <h1 class="cover-heading">Daftar sukses ! &#127881;</h1>
+			    <p class="lead">
+			    	Silahkan melakukan <Link to="/confirm"> Konfirmasi </Link> untuk mendapatkan link acara
+			    	<br /> (konfirmasi dibuka pada tanggal &#128197; [TANGGAL]) ^_^ jadi, <i>stay tune</i> yaaah &#128037;!
+			    </p>
+			 </main>
 		</div>
 	)
 }
